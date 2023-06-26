8a0d41d24391a90eb056bd0ebffe5393c96a6c1d
WinnieHalm <86756785+WinnieHalm@users.noreply.github.com>
2023-06-26 05:21:18 -0500

    Add files via upload

---
+++ Module 111.cpp
@@ -0,0 +1,14 @@
+/*
+Developer: Winifred Halm De-Souza
+Date: 2023-05-14
+Purpose: This program prints "Hello, World!" to the console.
+*/
+
+#include <iostream> // Include the necessary library for input/output operations
+
+int main() {
+    // Print "Hello, World!" to the console
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0; // Return 0 to indicate successful program execution
+}
---
+++ main 2.py
@@ -0,0 +1,8 @@
+"""
+Developer: Winifred Halm De-Souza
+Date: 13 May 2023
+Purpose: This application prints the message "Hello, World!" to the console.
+"""
+
+# Print the message "Hello, World!"
+print("Hello, World!")
